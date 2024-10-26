4d10c9ca2beb8bd11dcbd65e289332ab60345c42
boomoioi <n.nanthawisit@gmail.com>
2024-10-26 22:43:21 +0700

    Refactor app.module.ts and histories.module.ts

--- src/modules/histories/histories.module.ts
+++ src/modules/histories/histories.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { HistoriesService } from './histories.service';
 import { HistoriesController } from './histories.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../users/entities/user.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [HistoriesController],
   providers: [HistoriesService],
 })
-export class HistoriesModule {}
+export class HistoriesModule { }
